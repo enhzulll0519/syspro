20e1c08854b134c17855a4517351d93f7db97f06
enhzulll0519 <is21d008@mandakh.edu.mn>
2024-03-27 14:37:11 +0800

    2nddd

--- backend/apps/models.py
+++ backend/apps/models.py
@@ -7,5 +7,13 @@ class Todo(models.Model):
     isDone=models.BooleanField(default=False)
     date=models.DateTimeField(auto_now_add=True)
 
+class List(models.Model):
+    Нэр = models.CharField(max_length=50)
+    Тайлбар = models.CharField(max_length=200)
+    Огноо = models.DateTimeField(auto_now_add=True)
+    Хаяг = models.CharField(max_length=200)
+    Мөнгө = models.CharField(max_length=50)
+
+
     def __str__(self):
-        return self.title[:20]
+        return self.Нэр[:20]
