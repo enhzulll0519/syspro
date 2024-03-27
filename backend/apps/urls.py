b1ce4a91c86e0f15c7ec283bb5124152514e3310
enhzulll0519 <is21d008@mandakh.edu.mn>
2024-03-27 15:21:40 +0800

    2nddd

---
+++ backend/apps/urls.py
@@ -0,0 +1,9 @@
+from apps import views
+
+from django.urls import path
+from .views import TodoGetCreate, TodoUpdateDelete
+
+urlpatterns = [
+    path('', TodoGetCreate.as_view()),
+    path('<int:pk>', TodoUpdateDelete.as_view())
+]
--- backend/apps/views.py
+++ backend/apps/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializers
 # Create your views here.
+class TodoGetCreate(generics.ListCreateAPIView):
+    queryset=Todo.objects.all() 
+    serializer_class=TodoSerializers # Харах бүртгэх үйлдэл хийх боломжтой класс
+
+
+class TodoUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Todo.objects.all()
+    serializer_class=TodoSerializers
